c92800f5e5741d3069dbf70781a98cf5555ec15b
David Yang <yang.zhiyu@northeastern.edu>
2025-08-02 11:34:29 -0700

    update product page for missing product objects

--- products/views.py
+++ products/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from django.views import View
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Product
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -19,19 +20,36 @@ class ProductLandingPageView(TemplateView):
     template_name= "landing.html"
 
     def get_context_data(self, **kwargs):
-        product = Product.objects.get(name="Scans")
         context = super(ProductLandingPageView, self).get_context_data(**kwargs)
-        context.update({
-            "product": product,
-            "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
-        })
+        
+        try:
+            product = Product.objects.get(name="Scans")
+            context.update({
+                "product": product,
+                "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY,
+                "product_available": True
+            })
+        except ObjectDoesNotExist:
+            context.update({
+                "product": None,
+                "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY,
+                "product_available": False,
+                "error_message": "Products unavailable. Please set up your stripe product information in the database."
+            })
+        
         return context
 
 # Create your views here.
 class CreateCheckoutSessionView(View):
     def post (self, request, *args, **kwargs):
         product_id = self.kwargs["pk"]
-        product = Product.objects.get(id=product_id)
+        try:
+            product = Product.objects.get(id=product_id)
+        except ObjectDoesNotExist:
+            return JsonResponse({
+                'error': 'Product not found'
+            }, status=404)
+            
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=[
@@ -87,14 +105,17 @@ def stripe_webhook(request):
         customer_email = session['customer_details']['email']
         product_id = session['metadata']['product_id']
 
-        product = Product.objects.get(id=product_id)
-
-        send_mail(
-            subject="Here is your product",
-            message=f"Thanks for your purchase. The URL is {product.url}",
-            recipient_list=[customer_email],
-            from_email = "NCU@test.com"
-        )
+        try:
+            product = Product.objects.get(id=product_id)
+            send_mail(
+                subject="Here is your product",
+                message=f"Thanks for your purchase. The URL is {product.url}",
+                recipient_list=[customer_email],
+                from_email = "NCU@test.com"
+            )
+        except ObjectDoesNotExist:
+            print(f"Product with id {product_id} not found")
+            # You might want to log this error or handle it differently
 
 
     # For now, you only need to print out the webhook payload so you can see
